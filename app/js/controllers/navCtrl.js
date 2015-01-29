ee2731f9e2031c2ac845d6811458148be78d8bf8
Kyle Olson <kolson@four51.com>
2015-01-29 15:59:09 -0600

    PW-14396 Order/Cart - In IE, the cart icon persists after order submit.

--- app/js/controllers/navCtrl.js
+++ app/js/controllers/navCtrl.js
@@ -52,6 +52,6 @@ function ($location, $route, $scope, $451, User) {
 	}
 
 	$scope.$on('event:orderUpdate', function(event, order) {
-		$scope.cartCount = order ? (order.Status == 'Unsubmitted' || order.Status == 'AwaitingApproval') ? order.LineItems.length : null : null;
+		$scope.cartCount = order ? (order.Status == 'Unsubmitted') ? order.LineItems.length : null : null;
 	});
 }]);
